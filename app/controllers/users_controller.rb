29e5f2127694f6f0f7edd60003049dd7e38ace5d
Eddie Wen <405139216@qq.com>
2024-06-07 16:15:54 +0800

    HTML form finished.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,21 @@
 class UsersController < ApplicationController
   def create
+    @user = User.new(allowed_user_params)
+    if @user.save
+      redirect_to new_user_path
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
   
   def new
+    @user = User.new
   end
-  
+
+  private
+
+  def allowed_user_params
+    params.require(:user).permit(:username, :email, :password)
+  end
+
 end
