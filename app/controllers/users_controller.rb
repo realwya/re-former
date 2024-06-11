65afe3f73baea1c33a315748adba1fbb342869c4
Eddie Wen <405139216@qq.com>
2024-06-11 15:19:21 +0800

    added #index, #edit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,16 @@
 class UsersController < ApplicationController
+  def index
+    @users = User.all
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
   def create
     @user = User.new(allowed_user_params)
     if @user.save
-      redirect_to new_user_path
+      redirect_to @user
     else
       render :new, status: :unprocessable_entity
     end
@@ -12,10 +20,25 @@ class UsersController < ApplicationController
     @user = User.new
   end
 
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+
+    if @user.update(allowed_user_params)
+      redirect_to @user
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
   private
 
   def allowed_user_params
     params.require(:user).permit(:username, :email, :password)
   end
+
 
 end
